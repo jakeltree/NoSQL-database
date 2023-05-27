62e4160476832c4c3b922b5a013eb3341f638527
Vsevolod kalyakin <vsekalya@gmail.com>
2023-05-27 17:18:18 +0300

    2

---
+++ task3_build/CMakeFiles/task.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task.dir/src/task3.cpp.o"
+  "CMakeFiles/task.dir/src/task3.cpp.o.d"
+  "libtask.a"
+  "libtask.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
